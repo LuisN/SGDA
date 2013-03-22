3643fbe0dd6d63c073d176f9c25dc59867f6ad7c
shaggikpo@gmail.com <shaggikpo@gmail.com>
2013-03-22 03:08:16 -0300

    Agregando comentarios a SGDA.vb

---
+++ SGDA/SGDA/frmLogin.Designer.vb
@@ -0,0 +1,123 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmLogin
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmLogin))
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.BtnEntrar = New System.Windows.Forms.Button()
+        Me.lblStatus = New System.Windows.Forms.Label()
+        Me.btnTest = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtUsername
+        '
+        Me.txtUsername.Location = New System.Drawing.Point(22, 48)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(232, 20)
+        Me.txtUsername.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(19, 32)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(96, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Nombre de usuario"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(19, 71)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(61, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Contraseña"
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(22, 87)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtPassword.Size = New System.Drawing.Size(232, 20)
+        Me.txtPassword.TabIndex = 3
+        '
+        'BtnEntrar
+        '
+        Me.BtnEntrar.Location = New System.Drawing.Point(95, 113)
+        Me.BtnEntrar.Name = "BtnEntrar"
+        Me.BtnEntrar.Size = New System.Drawing.Size(75, 23)
+        Me.BtnEntrar.TabIndex = 4
+        Me.BtnEntrar.Text = "Entrar"
+        Me.BtnEntrar.UseVisualStyleBackColor = True
+        '
+        'lblStatus
+        '
+        Me.lblStatus.AutoSize = True
+        Me.lblStatus.Location = New System.Drawing.Point(21, 9)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(13, 13)
+        Me.lblStatus.TabIndex = 5
+        Me.lblStatus.Text = "a"
+        '
+        'btnTest
+        '
+        Me.btnTest.Location = New System.Drawing.Point(179, 113)
+        Me.btnTest.Name = "btnTest"
+        Me.btnTest.Size = New System.Drawing.Size(75, 23)
+        Me.btnTest.TabIndex = 6
+        Me.btnTest.Text = "Test"
+        Me.btnTest.UseVisualStyleBackColor = True
+        '
+        'frmLogin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(276, 145)
+        Me.Controls.Add(Me.btnTest)
+        Me.Controls.Add(Me.lblStatus)
+        Me.Controls.Add(Me.BtnEntrar)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtUsername)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "frmLogin"
+        Me.Text = "Login"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtUsername As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtPassword As System.Windows.Forms.TextBox
+    Friend WithEvents BtnEntrar As System.Windows.Forms.Button
+    Friend WithEvents lblStatus As System.Windows.Forms.Label
+    Friend WithEvents btnTest As System.Windows.Forms.Button
+
+End Class
